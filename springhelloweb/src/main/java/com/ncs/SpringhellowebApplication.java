217fe2b29120e4af5b5ebc707b2d962eea207d3c
shivaspk <shivaspk.siva@gmail.com>
2022-02-18 11:39:42 +0530

    initial

---
+++ springhelloweb/src/main/java/com/ncs/SpringhellowebApplication.java
@@ -0,0 +1,13 @@
+package com.ncs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringhellowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringhellowebApplication.class, args);
+	}
+
+}
